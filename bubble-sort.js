b9c8328fcb0730ab58d526654f37dfd56644ec38
Dhr_Brick <dengheran@aliyun.com>
2019-09-15 17:39:02 +0800

    udpate

---
+++ bubble-sort.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i; j < arr.length - i; j++) {
+            if (arr[i] > arr[j]) {
+                let temp = arr[j]
+                arr[j] = arr[i]
+                arr[i] = temp
+            }
+        }
+    }
+    return arr
+}
+
+console.log(bubbleSort([50, 37, 42, 28, 5, 72, 140]))
